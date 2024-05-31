9e8824dd033d5b1769a35df6f592dc8c89914133
Sacha Khosrowshahi <sacha.khosrowshahi@telecom-paris.fr>
2024-05-31 13:55:35 +0200

    modif

---
+++ src/siteweb/serveur/client.js
@@ -0,0 +1,79 @@
+//Partie socket
+const socket = new WebSocket('ws://localhost:8080');
+
+
+
+socket.onopen = function() {
+  console.log('Connexion établie.');
+};
+
+
+socket.onerror = function(error) {
+  console.error('Erreur WebSocket : ', error);
+};
+  
+socket.onclose = function() {
+  console.log('Connexion WebSocket fermée : ');
+};
+
+
+async function sendName() {
+  const name = document.getElementById('nameInput');
+  const message = {type: 'NAME', data: name.value};
+  socket.send(JSON.stringify(message));
+  name.value = ''; // Efface le champ d'entrée après l'envoi
+  name.disabled = true;
+  name.style.opacity = '0.5';
+}
+
+
+function requestAudioFromServer(){
+  const message = {type: 'REQUEST_AUDIO'};
+  socket.send(JSON.stringify(message));
+}
+
+
+
+//partie audio
+let audioChunks = [];
+let sound;
+
+socket.binaryType = 'arraybuffer';
+
+socket.onmessage = (event) => {
+  if (typeof event.data === 'string') {
+      const message = JSON.parse(event.data);
+      if (message.end) {
+          const blob = new Blob(audioChunks, { type: 'audio/mpeg' });
+          const url = URL.createObjectURL(blob);
+
+          sound = new Howl({
+            src: [url],
+            format: ['mp3'],
+            autoplay: true,
+          });
+
+          // Clean up the URL object after the audio is loaded
+          sound.on('load', () => {
+              URL.revokeObjectURL(url);
+          });
+      }
+  } else {
+      audioChunks.push(event.data);
+  }
+};
+
+
+function playAudio() {
+  sound.play();
+}
+function pauseAudio() {
+  sound.pause();
+}
+function stopAudio() {
+  sound.stop();
+}
+
+
+
+  
